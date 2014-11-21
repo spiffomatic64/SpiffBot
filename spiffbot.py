a50801547a73e6b2bfeacef8bf39bcaef817da4f
spiffomatic64 <root@spiffomatic64.com>
2014-11-21 03:12:08 -0500

    Moved html colors to new file, and fixed a few control bugs

--- spiffbot.py
+++ spiffbot.py
@@ -14,6 +14,7 @@ import win32api as win32
 import win32con
 import twitch_auth
 import string
+import html_colors
 
 #Prints to console, and a log file
 def printer(string):
@@ -46,7 +47,7 @@ def convertcolor(input):
     input = input.lower()
     stuff = []
     #look for html color
-    for key, value in sorted(colors.iteritems()):
+    for key, value in sorted(html_colors.colors.iteritems()):
         if input.find ( key.lower() ) != -1:
             stuff.append(bounds(int("0x"+value[0:2],0)))
             stuff.append(bounds(int("0x"+value[2:4],0)))
@@ -182,7 +183,7 @@ def get_viewers():
             viewers.append(viewer)
             printer(viewer)
     for viewer in output['chatters']['moderators']:
-        if viewer not in optout:
+        if viewer not in optout and viewer!="spiffbot" and viewer!="spiffomatic64":
             viewers.append(viewer)
             printer(viewer)
     return viewers
@@ -351,6 +352,7 @@ def master_commands(user,data):
     global master
     global irc
     global sounds
+    global warn_timer
     global ser
     global scare
     
@@ -662,7 +664,6 @@ def allleds(r,g,b):
 def user_commands(user,data):
     global master
     global irc
-    global colors
     global ser
     global optout
     global mode
@@ -716,7 +717,6 @@ def user_commands(user,data):
     #if data.find ( 'fire' ) != -1:
         #fire()
         #return
-    printer("yay")
     m = re.search('(\w+)\((.+(?:\|[a-zA-Z0-9#]+)*)\)',data,re.IGNORECASE)
     if m:
         printer("regex passed")
@@ -766,7 +766,7 @@ def user_commands(user,data):
                 printer("Invalid color: %s" % parts[0])
                 return
     #html color keys (single color, no animation)
-    for key, value in colors.iteritems():
+    for key, value in html_colors.colors.iteritems():
         if data.find ( key.lower() ) != -1:
             printer("key: %s value: %s : %s,%s,%s" % (key,value,int("0x"+value[0:2],0),int("0x"+value[2:4],0),int("0x"+value[4:6],0)))
             irc_msg("%s!!!" % key.upper())
@@ -774,151 +774,6 @@ def user_commands(user,data):
             time.sleep(5)
             modedefault()
             return
-
-#map of html color commands to hex codes
-colorsup = { "AliceBlue":"F0F8FF" ,
-"AntiqueWhite":"FAEBD7" ,
-"Aqua":"00FFFF" ,
-"Aquamarine":"7FFFD4" ,
-"Azure":"F0FFFF" ,
-"Beige":"F5F5DC" ,
-"Bisque":"FFE4C4" ,
-"Black":"000000" ,
-"BlanchedAlmond":"FFEBCD" ,
-"Blue":"0000FF" ,
-"BlueViolet":"8A2BE2" ,
-"Brown":"A52A2A" ,
-"BurlyWood":"DEB887" ,
-"CadetBlue":"5F9EA0" ,
-"Chartreuse":"7FFF00" ,
-"Chocolate":"D2691E" ,
-"Coral":"FF7F50" ,
-"CornflowerBlue":"6495ED" ,
-"Cornsilk":"FFF8DC" ,
-"Crimson":"DC143C" ,
-"Cyan":"00FFFF" ,
-"DarkBlue":"00008B" ,
-"DarkCyan":"008B8B" ,
-"DarkGoldenRod":"B8860B" ,
-"DarkGray":"A9A9A9" ,
-"DarkGreen":"006400" ,
-"DarkKhaki":"BDB76B" ,
-"DarkMagenta":"8B008B" ,
-"DarkOliveGreen":"556B2F" ,
-"DarkOrange":"FF8C00" ,
-"DarkOrchid":"9932CC" ,
-"DarkRed":"8B0000" ,
-"DarkSalmon":"E9967A" ,
-"DarkSeaGreen":"8FBC8F" ,
-"DarkSlateBlue":"483D8B" ,
-"DarkSlateGray":"2F4F4F" ,
-"DarkTurquoise":"00CED1" ,
-"DarkViolet":"9400D3" ,
-"DeepPink":"FF1493" ,
-"DeepSkyBlue":"00BFFF" ,
-"DimGray":"696969" ,
-"DodgerBlue":"1E90FF" ,
-"FireBrick":"B22222" ,
-"FloralWhite":"FFFAF0" ,
-"ForestGreen":"228B22" ,
-"Fuchsia":"FF00FF" ,
-"Gainsboro":"DCDCDC" ,
-"GhostWhite":"F8F8FF" ,
-"Gold":"FFD700" ,
-"GoldenRod":"DAA520" ,
-"Gray":"808080" ,
-"Green":"008000" ,
-"GreenYellow":"ADFF2F" ,
-"HoneyDew":"F0FFF0" ,
-"HotPink":"FF69B4" ,
-"IndianRed":"CD5C5C" ,
-"Indigo":"4B0082" ,
-"Ivory":"FFFFF0" ,
-"Khaki":"F0E68C" ,
-"Lavender":"E6E6FA" ,
-"LavenderBlush":"FFF0F5" ,
-"LawnGreen":"7CFC00" ,
-"LemonChiffon":"FFFACD" ,
-"LightBlue":"ADD8E6" ,
-"LightCoral":"F08080" ,
-"LightCyan":"E0FFFF" ,
-"LightGoldenRodYellow":"FAFAD2" ,
-"LightGray":"D3D3D3" ,
-"LightGreen":"90EE90" ,
-"LightPink":"FFB6C1" ,
-"LightSalmon":"FFA07A" ,
-"LightSeaGreen":"20B2AA" ,
-"LightSkyBlue":"87CEFA" ,
-"LightSlateGray":"778899" ,
-"LightSteelBlue":"B0C4DE" ,
-"LightYellow":"FFFFE0" ,
-"Lime":"00FF00" ,
-"LimeGreen":"32CD32" ,
-"Linen":"FAF0E6" ,
-"Magenta":"FF00FF" ,
-"Maroon":"800000" ,
-"MediumAquaMarine":"66CDAA" ,
-"MediumBlue":"0000CD" ,
-"MediumOrchid":"BA55D3" ,
-"MediumPurple":"9370DB" ,
-"MediumSeaGreen":"3CB371" ,
-"MediumSlateBlue":"7B68EE" ,
-"MediumSpringGreen":"00FA9A" ,
-"MediumTurquoise":"48D1CC" ,
-"MediumVioletRed":"C71585" ,
-"MidnightBlue":"191970" ,
-"MintCream":"F5FFFA" ,
-"MistyRose":"FFE4E1" ,
-"Moccasin":"FFE4B5" ,
-"NavajoWhite":"FFDEAD" ,
-"Navy":"000080" ,
-"OldLace":"FDF5E6" ,
-"Olive":"808000" ,
-"OliveDrab":"6B8E23" ,
-"Orange":"FFA500" ,
-"OrangeRed":"FF4500" ,
-"Orchid":"DA70D6" ,
-"PaleGoldenRod":"EEE8AA" ,
-"PaleGreen":"98FB98" ,
-"PaleTurquoise":"AFEEEE" ,
-"PaleVioletRed":"DB7093" ,
-"PapayaWhip":"FFEFD5" ,
-"PeachPuff":"FFDAB9" ,
-"Peru":"CD853F" ,
-"Pink":"FFC0CB" ,
-"Plum":"DDA0DD" ,
-"PowderBlue":"B0E0E6" ,
-"Purple":"800080" ,
-"Red":"FF0000" ,
-"RosyBrown":"BC8F8F" ,
-"RoyalBlue":"4169E1" ,
-"SaddleBrown":"8B4513" ,
-"Salmon":"FA8072" ,
-"SandyBrown":"F4A460" ,
-"SeaGreen":"2E8B57" ,
-"SeaShell":"FFF5EE" ,
-"Sienna":"A0522D" ,
-"Silver":"C0C0C0" ,
-"SkyBlue":"87CEEB" ,
-"SlateBlue":"6A5ACD" ,
-"SlateGray":"708090" ,
-"Snow":"FFFAFA" ,
-"SpringGreen":"00FF7F" ,
-"SteelBlue":"4682B4" ,
-"Tan":"D2B48C" ,
-"Teal":"008080" ,
-"Thistle":"D8BFD8" ,
-"Tomato":"FF6347" ,
-"Turquoise":"40E0D0" ,
-"Violet":"EE82EE" ,
-"Wheat":"F5DEB3" ,
-"White":"FFFFFF" ,
-"WhiteSmoke":"F5F5F5" ,
-"Yellow":"FFFF00" ,
-"YellowGreen":"9ACD32" }
-
-#turns all html colors to lowercase
-colors = dict((k.lower(), v) for k,v in colorsup.iteritems())
     
 #Map of sound commands to sound files
 sounds = { "slam" : "SOUND_1277.ogg",
@@ -1015,22 +870,24 @@ while True:
     parts = orig.split() #Split irc data by white space
     if orig.find ( 'PING' ) != -1: #Needed to keep connected to IRC, without this, twitch will disconnect
         irc.send ( 'PONG ' + orig.split() [ 1 ] + '\r\n' )
-    if len(parts)>3: #all user input data has at least 3 parts user, PRIVMSG, #channel
-        user = parts.pop(0) 
-        user = user[1:user.find("!")] #get the username from the first "part"
-        parts.pop(0) #throw away the next two parts
-        parts.pop(0)
-        #Put all parts back together into data variable, and lowercase it
-        data = ""
-        for part in parts:
-            data = data + part + " "
-        data = data.lower()
-        printer("User Message: %s" % data)
-        
-        #check for admin commands
-        admin_commands(user,data)
-        #if scary mode, check for in control commands
-        if mode == 0:
-            master_commands(user,data)  
-        #check for normal user commands
-        user_commands(user,data)
+    
+    if parts[1].lower()=="privmsg" and parts[2].lower()=="#spiffomatic64":
+        if len(parts)>3: #all user input data has at least 3 parts user, PRIVMSG, #channel
+            user = parts.pop(0) 
+            user = user[1:user.find("!")] #get the username from the first "part"
+            parts.pop(0) #throw away the next two parts
+            parts.pop(0)
+            #Put all parts back together into data variable, and lowercase it
+            data = ""
+            for part in parts:
+                data = data + part + " "
+            data = data.lower()
+            printer("User Message: %s" % data)
+            
+            #check for admin commands
+            admin_commands(user,data)
+            #if scary mode, check for in control commands
+            if mode == 0:
+                master_commands(user,data)  
+            #check for normal user commands
+            user_commands(user,data)
