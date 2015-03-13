f454ff18ca12e05d044c85808fbbafca21bf3a8b
Toyz <toyzz@live.com>
2015-03-13 16:42:43 -0700

    Added a custom commands plugin for none default commands

---
+++ Spiffbot/CustomCommands/Commands/SourceCommand.cs
@@ -0,0 +1,23 @@
+﻿using Spiff.Core;
+using Spiff.Core.API.Commands;
+
+namespace CustomCommands.Commands
+{
+    public class SourceCommand : ICommand
+    {
+        public override string CommandName
+        {
+            get { return "source"; }
+        }
+
+        public override string CommandInfo
+        {
+            get { return "This is a link to the source code on github"; }
+        }
+
+        public override void Run(string[] parts, string complete, string channel, string nick)
+        {
+            TwitchIRC.Instance.WriteOut.SendMessage(string.Format("Hey, {0} here is a link to the code: https://github.com/Toyz/SpiffBot/tree/master/Spiffbot", nick), channel);
+        }
+    }
+}
---
+++ Spiffbot/CustomCommands/CustomCommands.cs
@@ -0,0 +1,33 @@
+﻿using CustomCommands.Commands;
+using Spiff.Core.API;
+
+namespace CustomCommands
+{
+    public class CustomCommands : Plugin
+    {
+        public override string Name
+        {
+            get { return "Custom Commands"; }
+        }
+
+        public override string Author
+        {
+            get { return "Toyz"; }
+        }
+
+        public override string Description
+        {
+            get { return "A few custom commands for my stream"; }
+        }
+
+        public override void Start()
+        {
+            RegisterCommand(new SourceCommand());
+        }
+
+        public override void Destory()
+        {
+            
+        }
+    }
+}
