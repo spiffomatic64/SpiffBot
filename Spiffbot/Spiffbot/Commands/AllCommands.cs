5b586d2964d92d586e63c7039d9592c0596478fb
Toyz <toyzz@live.com>
2015-03-12 23:24:54 -0700

    Added basic Command System

---
+++ Spiffbot/Spiffbot/Commands/AllCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spiff.IRC;
+using Spiff.IRC.API.Commands;
+
+namespace Spiffbot.Commands
+{
+    public class AllCommands : ICommand
+    {
+        public override string CommandName
+        {
+            get { return "allcmd"; }
+        }
+
+        public override string CommandInfo
+        {
+            get { return "Returns a list of all possible commands"; }
+        }
+
+        public override void Run(string[] parts, string complete, string channel, string nick)
+        {
+            var commands = string.Join(", ", TwitchIRC.Instance.AllCommands().Keys);
+
+            TwitchIRC.Instance.WriteOut.SendMessage("All Commands: " + commands, channel);
+        }
+    }
+}
--- Spiffbot/Spiffbot/Program.cs
+++ Spiffbot/Spiffbot/Program.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Spiff.IRC;
+using Spiffbot.Commands;
 
 namespace Spiffbot
 {
     class Program
     {
+        private static TwitchIRC _server;
         static void Main(string[] args)
         {
+            _server = new TwitchIRC("channel", "Username", "Password");
+
+            LoadCommands();
+
+            _server.Start();
+        }
+
+        static void LoadCommands()
+        {
+            _server.AddCommand(new HelpCommand());
+            _server.AddCommand(new AllCommands());
         }
     }
 }
