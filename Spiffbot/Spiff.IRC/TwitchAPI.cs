d0ffa13319910617e1069f5aec1c6c3853c194ec
Toyz <toyzz@live.com>
2015-03-15 11:29:34 -0700

    Updated the API to add followers

--- Spiffbot/Spiff.IRC/TwitchAPI.cs
+++ Spiffbot/Spiff.IRC/TwitchAPI.cs
@@ -2,11 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Spiff.Core.Utils;
 
 namespace Spiff.Core
 {
+    public class TwitchJson
+    {
+        //Add All json responses here
+        public static class Followers
+        {
+            public class Links
+            {
+                public string self { get; set; }
+            }
+
+            public class Links2
+            {
+                public string self { get; set; }
+            }
+
+            public class User
+            {
+                public int _id { get; set; }
+                public string name { get; set; }
+                public string created_at { get; set; }
+                public string updated_at { get; set; }
+                public Links2 _links { get; set; }
+                public string display_name { get; set; }
+                public string logo { get; set; }
+                public string bio { get; set; }
+                public string type { get; set; }
+            }
+
+            public class Follow
+            {
+                public string created_at { get; set; }
+                public Links _links { get; set; }
+                public bool notifications { get; set; }
+                public User user { get; set; }
+            }
+
+            public class Links3
+            {
+                public string self { get; set; }
+                public string next { get; set; }
+            }
+
+            public class RootObject
+            {
+                public List<Follow> follows { get; set; }
+                public int _total { get; set; }
+                public Links3 _links { get; set; }
+            }
+        }
+    }
+
     public static class TwitchAPI
     {
         public class Viewer
@@ -62,6 +113,17 @@ namespace Spiff.Core
             }
 
             return users;
-        } 
+        }
+
+        public static TwitchJson.Followers.RootObject GetFollowers(string streamer)
+        {
+            using (var client = new WebClient())
+            {
+
+                return
+                    JsonConvert.DeserializeObject<TwitchJson.Followers.RootObject>(
+                        client.DownloadString("https://api.twitch.tv/kraken/channels/" + streamer + "/follows"));
+            }
+        }
     }
 }
